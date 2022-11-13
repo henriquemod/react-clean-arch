c604454aff40479e8b48e7f6002f4db69287ed0a
Henrique Souza <henrique.asouza@outlook.com>
2022-11-12 22:32:40 -0300

    test: ensure MinLengthValidation returns falsy if value is valid

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -7,4 +7,10 @@ describe('MinLengthValidation', () => {
     const error = sut.validate('')
     expect(error).toEqual(new InvalidFieldError())
   })
+
+  test('should falsy if value is valid', () => {
+    const sut = new MinLengthValidation('email', 5)
+    const error = sut.validate('123456')
+    expect(error).toBeFalsy()
+  })
 })
