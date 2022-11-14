960a66b86bab91d8d7852cd34e292ae17e79d8b9
Henrique Souza <henrique.asouza@outlook.com>
2022-11-14 14:16:12 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usecases/save-access-token/local-save-access-token.spec.ts
@@ -1,11 +1,24 @@
-import { SetStorageSpy } from '@/data/test/mock-storage'
+import { SetStorageSpy } from '@/data/test'
 import faker from 'faker'
 import { LocalSaveAccessToken } from './local-save-access-token'
 
+type SutTypes = {
+  sut: LocalSaveAccessToken
+  setStorage: SetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+  const setStorage = new SetStorageSpy()
+  const sut = new LocalSaveAccessToken(setStorage)
+  return {
+    sut,
+    setStorage
+  }
+}
+
 describe('LocalSaveAccessToken', () => {
   test('should call SetStorage with correct value', async () => {
-    const setStorage = new SetStorageSpy()
-    const sut = new LocalSaveAccessToken(setStorage)
+    const { sut, setStorage } = makeSut()
     const accessToken = faker.datatype.uuid()
     await sut.save(accessToken)
     expect(setStorage.key).toBe('accessToken')
