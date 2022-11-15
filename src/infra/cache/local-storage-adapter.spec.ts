68a57a89030617661e8064ef48339533c9507c91
Henrique Souza <henrique.asouza@outlook.com>
2022-11-15 16:34:48 -0300

    test: ensure LocalStorageAdapter throws if SetStorage throws

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -21,4 +21,13 @@ describe('LocalStorageAdapter', () => {
       expect(localStorage.setItem).toHaveBeenCalledWith(key, value)
     })
   })
+
+  test('should throw if localStorage throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(sut, 'set').mockRejectedValueOnce(new Error())
+    const key = faker.database.column()
+    const value = faker.random.word()
+    const promise = sut.set(key, value)
+    await expect(promise).rejects.toThrow(new Error())
+  })
 })
