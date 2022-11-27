ab2896e10790c59088ee9a246c29c6b55a8a3963
Henrique Souza <henrique.asouza@outlook.com>
2022-11-27 03:02:55 -0300

    test: ensure RemoteAddAccount throw UnexpectedError if HttpPostClient return 500

--- src/data/usecases/add-account/remove-add-account.spec.ts
+++ src/data/usecases/add-account/remove-add-account.spec.ts
@@ -52,4 +52,13 @@ describe('RemoveAddAccount', () => {
     const promise = sut.add(mockAddAccountParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('should throw UnexpectedError if HttpPostClient return 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
